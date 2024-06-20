035397bc0403ad66c0195b1df5dea2382010017c
Ada Lovelace <adalovelace@gmail.com>
2024-06-20 12:17:58 +0300

    added private and public keys

--- src/digital_signature/mod.rs
+++ src/digital_signature/mod.rs
@@ -1,7 +1,6 @@
+use rand::rngs::OsRng;
 use ring::digest::{self, Digest};
-use rsa::RsaPrivateKey;
-use rsa::RsaPublicKey;
-use rsa::{Hash, PaddingScheme, PublicKey};
+use rsa::{Hash, PaddingScheme, PublicKey, RsaPrivateKey, RsaPublicKey};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -16,7 +15,12 @@ impl User {
         let id = digest::digest(&digest::SHA256, self.name.as_bytes());
         id
     }
-    fn genarate_signature() {}
+    fn genarate_signature() {
+        let mut rng = OsRng;
+        let bits = 2048;
+        let private_key = RsaPrivateKey::new(&mut rng, bits).expect("Failed to generate a key");
+        let public_key = RsaPublicKey::from(&private_key);
+    }
 }
 
 pub fn digital_signature() {
